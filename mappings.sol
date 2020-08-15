dae8119f37cc1e4438a436668f28b659ae3b40b9
Ocean Yeung <oceanocean@gmail.com>
2020-08-14 18:25:10 -0700

    Basic Update

--- mappings.sol
+++ mappings.sol
@@ -3,29 +3,50 @@ pragma solidity 0.5.12;
 contract HelloWorld{
 
     struct Person {
-      address creator;
       uint id;
       string name;
       uint age;
       uint height;
+      address creator;
     }
-    
-    Person [] private people;
 
+    mapping (address => Person) private people;
+    
+    event personUpdated(address creator, string name, uint age, uint height);
+    
     function createPerson(string memory name, uint age, uint height) public {
+        Person memory person;
+        
         address creator = msg.sender;
 
-        Person memory newPerson;
-        newPerson.creator = creator;
-        newPerson.name = name;
-        newPerson.age = age;
-        newPerson.height = height;
+        person.creator = creator;
+        person.name = name;
+        person.age = age;
+        person.height = height;
         
-        people.push(newPerson);
-
+        people[msg.sender] = person;
     }
-    function getPerson(uint index) public view returns(string memory name, uint age, uint height){
-        require(index < people.length);
-        return (people[index].name, people[index].age, people[index].height);
+    
+    function updatePerson(string memory name, uint age, uint height) public {
+        address sender = msg.sender;
+        require(people[sender].creator == sender);
+        
+        people[sender].name = name;
+        people[sender].age = age;
+        people[sender].height = height;
+        
+        assert(
+            people[sender].creator == sender
+            && keccak256(abi.encodePacked(people[sender].name)) == keccak256(abi.encodePacked(name)) 
+            && people[sender].age == age 
+            && people[sender].height == height
+        );
+        
+        emit personUpdated(sender, name, age, height);
+    }
+    
+    function getPerson() public view returns(string memory name, uint age, uint height){
+        address creator = msg.sender;
+        return (people[creator].name, people[creator].age, people[creator].height);
     }
 }
