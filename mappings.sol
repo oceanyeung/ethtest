30661005437720b671c592243fe82653ed728f03
Ocean Yeung <oceanocean@gmail.com>
2020-08-17 01:01:09 -0700

    Refactored

--- mappings.sol
+++ mappings.sol
@@ -3,21 +3,21 @@ pragma solidity 0.5.12;
 contract HelloWorld{
 
     struct Person {
+      address creator;
       uint id;
       string name;
       uint age;
       uint height;
-      address creator;
     }
     
-    // Doubly linked list to track all the address that has created a person
     address owner;
 
-    mapping (address => Person) private people;
-    address[] creators;
-    uint creatorsCount;
+    mapping (address => Person) internal people;
+    Person[] internal peopleLookupById;
+    uint internal totalPeople;
     
-    event personUpdated(address creator, string name, uint age, uint height);
+    event personCreated(address creator, uint id, string name, uint age, uint height);
+    event personUpdated(address creator, string oldName, string newName, uint oldAge, uint newAge, uint oldHeight, uint newHeight);
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -29,27 +29,22 @@ contract HelloWorld{
     }
     
     function createPerson(string memory name, uint age, uint height) public {
-        Person memory person;
-        
         address creator = msg.sender;
 
-        person.creator = creator;
-        person.name = name;
-        person.age = age;
-        person.height = height;
-
-        if (people[msg.sender].creator != msg.sender) {
-            // record does not exist in people yet, so can increase count
-            creators.push(creator);
-            creatorsCount++;
-        }
-        person.id = creatorsCount;
-        people[msg.sender] = person;
+        Person memory person = Person(creator, ++totalPeople, name, age, height);
+        peopleLookupById.push(person);
+        people[creator] = person;
+        
+        emit personCreated(creator, person.id, person.name, person.age, person.height );
     }
     
     function updatePerson(string memory name, uint age, uint height) public {
         address sender = msg.sender;
         require(people[sender].creator == sender);
+        
+        string memory oldName = people[sender].name;
+        uint oldAge = people[sender].age;
+        uint oldHeight = people[sender].height;
         
         people[sender].name = name;
         people[sender].age = age;
@@ -62,7 +57,7 @@ contract HelloWorld{
             && people[sender].height == height
         );
         
-        emit personUpdated(sender, name, age, height);
+        emit personUpdated(sender, oldName, name, oldAge, age, oldHeight, height);
     }
     
     function getPerson() public view returns(string memory name, uint age, uint height){
@@ -71,9 +66,9 @@ contract HelloWorld{
     }
     
     function getAllPeople() view public onlyOwner returns(uint[] memory peopleIds) {
-        uint[] memory ids = new uint[](creatorsCount);
-        for (uint32 i=0; i < creatorsCount; i++) {
-            ids[i] = people[creators[i]].id;
+        uint[] memory ids = new uint[](totalPeople);
+        for (uint32 i=0; i < totalPeople; i++) {
+            ids[i] = peopleLookupById[i].id;
         }
         return ids;
     }
